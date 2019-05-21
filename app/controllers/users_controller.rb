aad4feac381c60ebe775bba7085ca914f5315bea
goodjobbin85 <td1729@hotmail.com>
2019-05-21 19:59:50 +0000

    user crud

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,49 @@ class UsersController < ApplicationController
         @user = User.find(params[:id])
     end
     
+    def new
+        @user = User.new
+    end
+    
+    def create 
+        @user = User.new(user_params)
+        if @user.save
+            flash[:success] = "User Successfully saved"
+            redirect_to user_path(@user)
+        else
+            render :new
+        end
+    end
+    
+    def edit
+        @user = User.find(params[:id])
+    end 
+    
+    def update
+        @user = User.find(params[:id])
+        @user.update(user_params)
+        if @user.save
+            flash[:success] = "User successfully updated"
+            redirect_to root_path
+        else
+            render :edit 
+        end
+    end
+    
+    def destroy
+        @user = User.find(params[:id])
+         if @user 
+             @user.destroy 
+             flash[:success] = "User successfully removed"
+             redirect_to users_path
+         end
+    end
+    
     private
+    
+    def user_params
+        params.require(:user).permit(:name, :username, :email, :password, :password_confirmation)
+    end
    
    
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
+    validates :name, :email, :username, presence: :true
+    
     has_secure_password
 end
